e45ace47bb3513dd8e8a4cd885cbd54999b5b6d0
Lee Gould <gould.lee@googlemail.com>
2019-03-26 11:11:23 +0000

    trying to get the change in date to load

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,6 @@
-import { Moment } from 'moment';
+import moment, { Moment } from 'moment';
 import RNCalendarEvents from 'react-native-calendar-events';
-import { Week, Day } from '../types';
+import { Day } from '../types';
 
 export const CHANGE_WEEK_DATE = 'CHANGE_WEEK_DATE';
 export const EVENTS_FETCH_STARTED = 'EVENTS_FETCH_STARTED';
@@ -13,18 +13,41 @@ export const changeWeekDate = (date: Moment) => {
         dispatch(eventsFetchStarted());
 
         try {
-            const week = {
-                days: await Promise.all([...Array(7).keys()].map(async i => {
-                    const adate = date.add(i, 'days');
-                    return ({
-                        date: adate,
-                        events: await RNCalendarEvents.fetchAllEvents(
-                            adate.clone().startOf(),
-                            adate.clone().endOf()
-                        ),
-                    } as Day);
-                })),
+            const days = [];
+            for (let i = 0;i < 7;i++) {
+                const aDate = moment(date).add(i, 'days');
+                console.log('aDate', aDate);
+                days.push({
+                    date: aDate,
+                    events: await RNCalendarEvents.fetchAllEvents(
+                        aDate.clone().startOf(),
+                        aDate.clone().endOf()
+                    ),
+                } as Day);
             };
+
+            // const events = await RNCalendarEvents.fetchAllEvents(
+            //     days[0].date.clone().startOf(),
+            //     days[6].date.clone().endOf()
+            // );
+
+            // for (let i = 0;i < events.length;i++) {
+            //     const event = events[i];
+            //     console.log('event', event);
+            //     const eventDate = moment(event.startDate).date();
+            //     for(let j = 0; j < days.length;j++) {
+            //         if (eventDate === days[j].date.date()) {
+            //             days[j].events.push(event);
+            //             break;
+            //         }
+            //     }
+            // }
+
+            const week = {
+                days
+            };
+
+            console.log('week', week);
 
             dispatch(eventsFetchSuccess(week));
         } catch (err) {
