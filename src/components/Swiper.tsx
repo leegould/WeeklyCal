d096c7f28349d160030a97b2d0cff799a25ef223
Lee Gould <gould.lee@googlemail.com>
2019-04-02 10:14:21 +0100

    changing date on drag

--- src/components/Swiper.tsx
+++ src/components/Swiper.tsx
@@ -1,6 +1,5 @@
 import React, { PureComponent } from 'react';
 import { StyleSheet, Animated, PanResponder } from 'react-native';
-// import Swiper from 'react-native-swiper';
 import Week from './Week';
 import { WeekState } from '../types';
 
@@ -15,9 +14,8 @@ type Props = {
 type State = {
     index: number,
     zone: string,
+    updating: boolean,
 };
-
-// const { width, height } = Dimensions.get("window");
 
 export default class NavigationSwiper extends PureComponent<Props, State> {
     static navigationOptions = {
@@ -32,12 +30,12 @@ export default class NavigationSwiper extends PureComponent<Props, State> {
         let dragDirection = '';
       
         if (draggedDown || draggedUp) {
-          if (draggedDown) dragDirection += 'down '
+          if (draggedDown) dragDirection += 'down'
           if (draggedUp) dragDirection +=  'up';
         }
       
         if (draggedLeft || draggedRight) {
-          if (draggedLeft) dragDirection += 'left '
+          if (draggedLeft) dragDirection += 'left'
           if (draggedRight) dragDirection +=  'right';
         }
       
@@ -52,63 +50,39 @@ export default class NavigationSwiper extends PureComponent<Props, State> {
         this.state = {
             index: 1,
             zone: '',
+            updating: false,
         }
         this.position = new Animated.ValueXY({ x: 0, y: 0 });
         this.panResponder = PanResponder.create({
             onMoveShouldSetPanResponder: (evt, gestureState) => { return Math.abs(gestureState.dx) >= 1 || Math.abs(gestureState.dy) >= 1 },//true,
-            onPanResponderMove: (evt, gestureState) => {
-                // console.log("I was moved")
-                // this.position.setValue({ x: gestureState.moveX, y: gestureState.moveY })
+            onPanResponderMove: async (evt, gestureState) => {
                 const drag = NavigationSwiper.getDirectionAndColor(gestureState);
-                // console.log('drag', drag);
                 if (drag) {
-                    console.log('drag', drag);
-                    this.setState({ zone: drag });
+                    console.log(`drag '${drag}'`);
+                    if(!this.state.updating) {
+                        if (drag === 'right') {
+                            console.log('drag.right', drag);
+                            this.props.onChangeDate(this.props.data.week.days[0].date.add(-7, 'days'));
+                        }
+                        if (drag === 'left') {
+                            console.log('drag.left', drag);
+                            this.props.onChangeDate(this.props.data.week.days[0].date.add(7, 'days'));
+                        }
+                        await this.setState({ updating: false });
+                    } else {
+                        console.log('drag.updating', drag);
+                        await this.setState({ zone: drag, updating: true });
+                    }
                 }
             },
         });
     }
 
-    // onIndexChanged = async (i:number) => {
-    //     const loopedUp = i === 0 && this.state.index === 2;
-    //     const loopedDown = i === 2 && this.state.index === 0;
-    //     const isIncreased = loopedUp || (!loopedDown && i > this.state.index);
-    //     // const isIncreased = (i > this.state.index && i !== 2) || (i === 0 && this.state.index === 2);
-    //     // const isIncreased = (this.state.index === 2 && i === 0) || (this.state.index !== 0 && i !== 2) || i > this.state.index;
-    //     let days = 7;
-    //     if (!isIncreased) {
-    //         days = -7;
-    //     }
-    //     console.log('onIndexChanged', i, this.state.index, days);
-    //     console.log(`up:${loopedUp}, down:${loopedDown} isIncreased:${isIncreased}`)
-    //     this.props.onChangeDate(this.props.data.week.days[0].date.add(days, 'days'));
-    //     await this.setState({index: i});
-    // }
-
-    // onMomentumScrollEnd = (e, state, context) => {
-    //     // this.props.onChangeDate(this.props.data.week.days[0].date.add(7, 'days'));
-    //     console.log('onMomentumScrollEnd', e, state, context.state);
-    // }
-
     render(){
         return (
-            // <Swiper
-            //     style={styles.wrapper}
-            //     index={1}
-            //     onIndexChanged={this.onIndexChanged}
-            //     showsButtons={false}
-            //     // showsPagination={false}
-            // >
-            // <View style={styles.slide1}>
-            //     <Week navigation={this.props.navigation} data={this.props.data}/>
-            // </View>
             <Animated.View style={styles.slide2} {...this.panResponder.panHandlers}>
                 <Week navigation={this.props.navigation} data={this.props.data}/>
             </Animated.View>
-            // <View style={styles.slide3}>
-            //     <Week navigation={this.props.navigation} data={this.props.data}/>
-            // </View>
-            // </Swiper>
         );
     }
 }
