f4d8d59c49e5a1b28a144f512a16832b2b84a89c
Lee Gould <gould.lee@googlemail.com>
2019-04-27 12:25:32 +0100

    dismissable background on event

--- src/components/Event.tsx
+++ src/components/Event.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInput, View, Text, StyleSheet, Switch, Animated, KeyboardAvoidingView } from 'react-native';
+import { TextInput, View, Text, StyleSheet, Switch, Animated, KeyboardAvoidingView, TouchableOpacity } from 'react-native';
 import moment, { Moment } from 'moment';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
@@ -89,144 +89,149 @@ export default class Add extends React.PureComponent<Props, State> {
 
         return (
             <KeyboardAvoidingView style={{flex: 1, backgroundColor: '#00000080'}} behavior='padding'>
-                <Formik
-                    initialValues={{
-                        title: event.title ? event.title : '',
-                        startDate: event.startDate ? moment(event.startDate) : moment(date).hours(10).minute(0).second(0),
-                        endDate: event.endDate ? moment(event.endDate) : moment(date).hours(16).minute(0).second(0),
-                        // allDay: true,
-                    }}
-                    validationSchema={ValidationSchema}
-                    onSubmit={(values: Values, formikActions) => {
-                        console.log('values', values);
-                        setTimeout(() => {
-                            const event: CalendarEvent = {
-                                title: values.title,
-                                startDate: values.startDate.toDate(),
-                                allDay: this.state.allDay,
-                                endDate: undefined,
-                            };
-
-                            if (!this.state.allDay) {
-                                event.allDay = false;
-                                event.endDate = values.endDate.toDate();
-                            }
-
-                            if (existingId) {
-                                event.id = existingId;
-                                this.props.onEditEvent(event);
-                            } else {
-                                this.props.onAddEvent(event);
-                            }
-                            
-                            formikActions.setSubmitting(false); // turn off disabled
-
-                            this.props.navigation.goBack();
-                        }, 500);
-                    }}
+                <TouchableOpacity 
+                    style={{flex: 1}}
+                    onPressOut={() => {this.props.navigation.goBack()}}
                 >
-                    {props => (
-                        <View style={styles.container}>
-                            <View style={styles.subcontainer}>
-                                <View style={styles.rowContainer}>
-                                    <Text style={styles.text}>All Day</Text>
-                                    <Switch
-                                        onValueChange={this.toggleAllDay}
-                                        value={this.state.allDay}
-                                        trackColor={{true: '#C2272D', false: ''}}
-                                        style={styles.switchInput}
+                    <Formik
+                        initialValues={{
+                            title: event.title ? event.title : '',
+                            startDate: event.startDate ? moment(event.startDate) : moment(date).hours(10).minute(0).second(0),
+                            endDate: event.endDate ? moment(event.endDate) : moment(date).hours(16).minute(0).second(0),
+                            // allDay: true,
+                        }}
+                        validationSchema={ValidationSchema}
+                        onSubmit={(values: Values, formikActions) => {
+                            console.log('values', values);
+                            setTimeout(() => {
+                                const event: CalendarEvent = {
+                                    title: values.title,
+                                    startDate: values.startDate.toDate(),
+                                    allDay: this.state.allDay,
+                                    endDate: undefined,
+                                };
+
+                                if (!this.state.allDay) {
+                                    event.allDay = false;
+                                    event.endDate = values.endDate.toDate();
+                                }
+
+                                if (existingId) {
+                                    event.id = existingId;
+                                    this.props.onEditEvent(event);
+                                } else {
+                                    this.props.onAddEvent(event);
+                                }
+                                
+                                formikActions.setSubmitting(false); // turn off disabled
+
+                                this.props.navigation.goBack();
+                            }, 500);
+                        }}
+                    >
+                        {props => (
+                            <View style={styles.container}>
+                                <View style={styles.subcontainer}>
+                                    <View style={styles.rowContainer}>
+                                        <Text style={styles.text}>All Day</Text>
+                                        <Switch
+                                            onValueChange={this.toggleAllDay}
+                                            value={this.state.allDay}
+                                            trackColor={{true: '#C2272D', false: ''}}
+                                            style={styles.switchInput}
+                                        />
+                                    </View>
+                                    <View style={styles.rowContainer}>
+                                        <Text style={styles.text}>{this.state.allDay ? 'Date' : 'From'}</Text>
+                                        <DateTimeButton
+                                            showTime={this.state.allDay}
+                                            date={props.values.startDate}
+                                            onDateChanged={(date: Moment) => {
+                                                props.handleChange('startDate');
+                                                props.values.startDate = date;
+                                            }}
+                                        />
+                                    </View>
+                                    {props.touched.startDate && props.errors.startDate ?
+                                    <Text style={styles.errorText} >
+                                        {props.errors.startDate}
+                                    </Text>
+                                    : null }
+                                    <Animated.View style={
+                                        [styles.rowContainer,
+                                            {
+                                                opacity: this.state.anim,
+                                                height: this.state.anim.interpolate({
+                                                    inputRange: [0, 1],
+                                                    outputRange: [0, 40]
+                                                })
+                                            }
+                                        ]}
+                                    >
+                                        <Text style={styles.text}>To</Text>
+                                        <DateTimeButton
+                                            showTime={this.state.allDay}
+                                            date={props.values.endDate}
+                                            onDateChanged={(date: Moment) => {
+                                                props.handleChange('endDate');
+                                                props.values.endDate = date;
+                                            }}
+                                        />
+                                    </Animated.View>
+                                    {!this.state.allDay && props.touched.endDate && props.errors.endDate ?
+                                    <Text style={styles.errorText} >
+                                        {props.errors.endDate}
+                                    </Text>
+                                    : null }
+                                    <TextInput
+                                        style={styles.textInput}
+                                        onChangeText={props.handleChange('title')}
+                                        onBlur={props.handleBlur('title')}
+                                        value={props.values.title}
+                                        placeholder="Title"
+                                        autoFocus
                                     />
-                                </View>
-                                <View style={styles.rowContainer}>
-                                    <Text style={styles.text}>{this.state.allDay ? 'Date' : 'From'}</Text>
-                                    <DateTimeButton
-                                        showTime={this.state.allDay}
-                                        date={props.values.startDate}
-                                        onDateChanged={(date: Moment) => {
-                                            props.handleChange('startDate');
-                                            props.values.startDate = date;
-                                        }}
-                                    />
-                                </View>
-                                {props.touched.startDate && props.errors.startDate ?
-                                <Text style={styles.errorText} >
-                                    {props.errors.startDate}
-                                </Text>
-                                : null }
-                                <Animated.View style={
-                                    [styles.rowContainer,
-                                        {
-                                            opacity: this.state.anim,
-                                            height: this.state.anim.interpolate({
-                                                inputRange: [0, 1],
-                                                outputRange: [0, 40]
-                                            })
+                                    {props.touched.title && props.errors.title ?
+                                    <Text style={styles.errorText} >
+                                        {props.errors.title}
+                                    </Text>
+                                    : null }
+                                    <View style={styles.rowButtons}>
+                                        {existingId && allowsUpdate &&
+                                        <Button
+                                            onPress={() => console.log('delete.press', event)}
+                                            title='Delete'
+                                            titleStyle={styles.buttonTitle}
+                                            icon={<Icon name='delete-forever' type='material-community' color='#C2272D' size={20} />}                                   
+                                            type='solid'
+                                            buttonStyle={styles.button}
+                                        />
                                         }
-                                    ]}
-                                >
-                                    <Text style={styles.text}>To</Text>
-                                    <DateTimeButton
-                                        showTime={this.state.allDay}
-                                        date={props.values.endDate}
-                                        onDateChanged={(date: Moment) => {
-                                            props.handleChange('endDate');
-                                            props.values.endDate = date;
-                                        }}
-                                    />
-                                </Animated.View>
-                                {!this.state.allDay && props.touched.endDate && props.errors.endDate ?
-                                <Text style={styles.errorText} >
-                                    {props.errors.endDate}
-                                </Text>
-                                : null }
-                                <TextInput
-                                    style={styles.textInput}
-                                    onChangeText={props.handleChange('title')}
-                                    onBlur={props.handleBlur('title')}
-                                    value={props.values.title}
-                                    placeholder="Title"
-                                    autoFocus
-                                />
-                                {props.touched.title && props.errors.title ?
-                                <Text style={styles.errorText} >
-                                    {props.errors.title}
-                                </Text>
-                                : null }
-                                <View style={styles.rowButtons}>
-                                    {existingId && allowsUpdate &&
-                                    <Button
-                                        onPress={() => console.log('delete.press', event)}
-                                        title='Delete'
-                                        titleStyle={styles.buttonTitle}
-                                        icon={<Icon name='delete-forever' type='material-community' color='#C2272D' size={20} />}                                   
-                                        type='solid'
-                                        buttonStyle={styles.button}
-                                    />
-                                    }
-                                    <Button
-                                        onPress={() => this.props.navigation.goBack()}
-                                        title='Cancel'
-                                        titleStyle={styles.buttonTitle}
-                                        icon={<Icon name='close-circle' type='material-community' color='#C2272D' size={20} />}                                   
-                                        type='solid'
-                                        buttonStyle={styles.button}
-                                    />
-                                    {allowsUpdate &&
-                                    <Button
-                                        onPress={props.handleSubmit as any}
-                                        title={existingId ? 'Save' : 'Add'}
-                                        disabled={props.isSubmitting}
-                                        titleStyle={styles.buttonTitle}
-                                        icon={<Icon name='calendar-plus' type='material-community' color='#C2272D' size={20} />}
-                                        type='solid'
-                                        buttonStyle={styles.button}
-                                    />
-                                    }
+                                        <Button
+                                            onPress={() => this.props.navigation.goBack()}
+                                            title='Cancel'
+                                            titleStyle={styles.buttonTitle}
+                                            icon={<Icon name='close-circle' type='material-community' color='#C2272D' size={20} />}                                   
+                                            type='solid'
+                                            buttonStyle={styles.button}
+                                        />
+                                        {allowsUpdate &&
+                                        <Button
+                                            onPress={props.handleSubmit as any}
+                                            title={existingId ? 'Save' : 'Add'}
+                                            disabled={props.isSubmitting}
+                                            titleStyle={styles.buttonTitle}
+                                            icon={<Icon name='calendar-plus' type='material-community' color='#C2272D' size={20} />}
+                                            type='solid'
+                                            buttonStyle={styles.button}
+                                        />
+                                        }
+                                    </View>
                                 </View>
                             </View>
-                        </View>
-                    )}
-                </Formik>
+                        )}
+                    </Formik>
+                </TouchableOpacity>
             </KeyboardAvoidingView>
         );
     }
