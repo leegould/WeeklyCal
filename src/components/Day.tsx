ad3bdccf1e8f7cf555d1b6b2d36bc67165c702a2
Lee Gould <gould.lee@googlemail.com>
2019-05-20 10:03:42 +0100

    Expanding a day so you can view it all

--- src/components/Day.tsx
+++ src/components/Day.tsx
@@ -12,7 +12,8 @@ type Props = {
     options: OptionsState,
     navigation: {
         navigate: Function,
-    }
+    },
+    onExpand: Function,
 };
 
 type State = {
@@ -74,8 +75,9 @@ export default class DayEvents extends Component<Props, State> {
                 {this.props.options.dayAddLink &&
                 <TouchableHighlight onPress={() => this.onAdd()} underlayColor={'beige'} hitSlop={{top: 10, left: 10, bottom: 10, right: 10}}>
                     <View style={styles.row}>
-                        <Animated.Text style={[styles.header, {opacity: this.state.fade}]}>{`${moment(this.props.day.date).format('ddd')} ${moment(this.props.day.date).format('DD')}`}</Animated.Text>
                         <Icon name='calendar-plus' type='material-community' color='green' onPress={() => this.onAdd()} size={18} />
+                        <Animated.Text style={[styles.header, {opacity: this.state.fade}]}>{`${moment(this.props.day.date).format('ddd')} ${moment(this.props.day.date).format('DD')}`}</Animated.Text>
+                        <Icon name='arrow-expand' type='material-community' color='green' onPress={() => this.props.onExpand(this.props.day)} size={18} />
                     </View>
                 </TouchableHighlight>
                 }
--- src/components/Week.tsx
+++ src/components/Week.tsx
@@ -1,7 +1,7 @@
 import React, { PureComponent } from 'react';
 import { StyleSheet, View, Animated } from 'react-native';
-import { WeekState, OptionsState } from '../types';
-import Day from './Day';
+import { WeekState, OptionsState, Day } from '../types';
+import DayComponent from './Day';
 
 type Props = {
     navigation: {
@@ -12,69 +12,72 @@ type Props = {
 };
 
 type State = {
-    fade: Animated.Value,
+    dayLayout: Animated.Value,
+    selectedDay: Day | null,
 };
 
 export default class Week extends PureComponent<Props, State> {
     constructor(props: Props) {
         super(props);
         this.state = {
-            fade: new Animated.Value(1),
+            dayLayout: new Animated.Value(1),
+            selectedDay: null,
         }
     }
 
-    componentDidUpdate() {
-        if (this.props.data.isFetching) {
-            this.fadeOut();
-        } else {
-            this.fadeIn();
-        }
-    }
+    onSelectDay(day: Day) {
+        // console.log('onSelectDay', day);
+        this.setState((previousState:State) => {
+            if (previousState.selectedDay === null) {
+                Animated.timing(this.state.dayLayout, 
+                {
+                    toValue: 0,
+                    duration: 250,
+                }).start();
+                return { selectedDay: day };
 
-    fadeIn() {
-        this.state.fade.setValue(0);
-        Animated.timing(                  
-            this.state.fade,            
-            {
-                toValue: 1,                   
-                duration: 150,              
+            } else {
+                Animated.timing(this.state.dayLayout,
+                {
+                    toValue: 1,
+                    duration: 250,
+                }).start();
+                return { selectedDay: null };
             }
-        ).start();
-    }
-
-    fadeOut() {
-        this.state.fade.setValue(1);
-        Animated.timing(                  
-            this.state.fade,            
-            {
-                toValue: 0,                   
-                duration: 150,              
-            }
-        ).start();
+        });
     }
 
     render() {
         const { data, navigation, options } = this.props;
 
+        const invertedOpacity = this.state.dayLayout.interpolate({ inputRange: [0, 1], outputRange: [1, 0] });
+
         return (
             <View style={styles.container}>
-                <View style={styles.main}>
+                {!this.state.selectedDay &&
+                <Animated.View style={[styles.main, { opacity: this.state.dayLayout } ]}>
                     <View style={styles.firstRow}>
-                        <Day day={data.week.days[0]} isToday isFetching={data.isFetching} navigation={navigation} options={options} />
+                        <DayComponent day={data.week.days[0]} isToday isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)}/>
                     </View>
                     <View style={styles.row}>
                         <View style={styles.col}>
-                            <Day day={data.week.days[1]} isFetching={data.isFetching} navigation={navigation} options={options} />
-                            <Day day={data.week.days[3]} isFetching={data.isFetching} navigation={navigation} options={options} />
-                            <Day day={data.week.days[5]} isFetching={data.isFetching} navigation={navigation} options={options} />
+                            <DayComponent day={data.week.days[1]} isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)} />
+                            <DayComponent day={data.week.days[3]} isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)}/>
+                            <DayComponent day={data.week.days[5]} isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)}/>
                         </View>
                         <View style={styles.col}>
-                            <Day day={data.week.days[2]} isFetching={data.isFetching} navigation={navigation} options={options} />
-                            <Day day={data.week.days[4]} isFetching={data.isFetching} navigation={navigation} options={options} />
-                            <Day day={data.week.days[6]} isFetching={data.isFetching} navigation={navigation} options={options} />
+                            <DayComponent day={data.week.days[2]} isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)}/>
+                            <DayComponent day={data.week.days[4]} isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)}/>
+                            <DayComponent day={data.week.days[6]} isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)}/>
                         </View>
                     </View>
-                </View>
+                </Animated.View>
+                }
+                {this.state.selectedDay &&
+                <Animated.View style={[styles.main, styles.singleDay, { opacity: invertedOpacity } ]}>
+                    <DayComponent day={this.state.selectedDay} isFetching={data.isFetching} navigation={navigation} options={options} onExpand={(day: Day) => this.onSelectDay(day)}/>
+                </Animated.View>
+                }
             </View>
         );
     }
@@ -91,6 +94,10 @@ const styles = StyleSheet.create({
     main: {
         flex: 1,
         flexDirection: 'column',
+    },
+    singleDay: {
+        backgroundColor: 'gray',
+        padding: 5,
     },
     row: {
         flex: 3,
