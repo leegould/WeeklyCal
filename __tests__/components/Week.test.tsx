f052eca7b6c8afd309268052ba4b7c129043a69b
Lee Gould <gould.lee@googlemail.com>
2019-06-06 09:32:19 +0100

    last component tests

---
+++ __tests__/components/Week.test.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import moment from 'moment';
+import renderer from 'react-test-renderer';
+import Week from '../../src/components/Week';
+
+jest.useFakeTimers();
+
+test('renders correctly with expected args', () => {
+    const props = {
+        navigation: {
+            navigate: () => {},
+        },
+        options: {
+            resetDate: false,
+            eventColor: false,
+            dayAddLink: false,
+            rollingWeek: false,
+            inlineAdd: false,
+            eventRowBorder: false,
+        },
+        data: {
+            isFetching: false,
+            week: {
+                days: [
+                    {
+                        date: moment('25/12/2005', 'DD/MM/YYYY').toDate(),
+                        events: [],
+                    },
+                    {
+                        date: moment('26/12/2005', 'DD/MM/YYYY').toDate(),
+                        events: [],
+                    },
+                    {
+                        date: moment('27/12/2005', 'DD/MM/YYYY').toDate(),
+                        events: [],
+                    },
+                    {
+                        date: moment('28/12/2005', 'DD/MM/YYYY').toDate(),
+                        events: [],
+                    },
+                    {
+                        date: moment('29/12/2005', 'DD/MM/YYYY').toDate(),
+                        events: [],
+                    },
+                    {
+                        date: moment('30/12/2005', 'DD/MM/YYYY').toDate(),
+                        events: [],
+                    },
+                    {
+                        date: moment('31/12/2005', 'DD/MM/YYYY').toDate(),
+                        events: [],
+                    },
+                ],
+            },
+            calendars: {
+                showAll: false,
+                selectedCalendars: [],
+                rollingWeek: false,
+                inlineAdd: false,
+                eventRowBorder: false,
+            }
+        },
+        onAddEvent: () => {},
+    }
+
+    const tree = renderer.create(
+        <Week {...props} />
+    ).toJSON();
+
+    expect(tree).toMatchSnapshot();
+});
